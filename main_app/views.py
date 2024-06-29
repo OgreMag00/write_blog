beb81f8a308357894b423a3e149edeb83663d64f
ogremag00 <ogremag00@bk.ru>
2024-06-29 10:04:06 -0800

    main_app show posts

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from profile_app.models import Text_post
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'main_app/index.html')
+    username = request.user.username
+    context = {'posts':Text_post.objects.filter(was_published=True).exclude(username=username)}
+    print(context)
+    return render(request, 'main_app/index.html', context=context)
